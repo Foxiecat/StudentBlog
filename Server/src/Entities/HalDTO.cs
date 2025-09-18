c9b0cae3c611100ef209f2840ee52862eb5f36e0
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-09-18 10:54:11 +0200

    Method is optional

--- Server/src/Entities/HalDTO.cs
+++ Server/src/Entities/HalDTO.cs
@@ -9,5 +9,5 @@ public record Link
 {
     public required string? Href { get; set; }
     public required string Rel { get; set; }
-    public required string Method { get; set; }
+    public string? Method { get; set; }
 }
