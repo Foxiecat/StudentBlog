07301006dad430835ef042537f03fcd46b7f0031
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-09-18 10:53:27 +0200

    Added documentation

--- Server/src/Features/Users/Endpoints/UserRegistrationEndpoint.cs
+++ Server/src/Features/Users/Endpoints/UserRegistrationEndpoint.cs
@@ -6,6 +6,21 @@ using src.Features.Users.DTOs;
 
 namespace src.Features.Users.Endpoints;
 
+/// <summary>
+/// Represents an endpoint responsible for handling user registration functionality.
+/// </summary>
+/// <remarks>
+/// This endpoint processes incoming requests to register a new user, validates the data,
+/// hashes the password, assigns necessary properties, saves the user entity to the repository,
+/// and generates a response containing user details and related links.
+/// </remarks>
+/// <example>
+/// - HTTP Method: POST
+/// - URL: /api/auth/register
+/// - Security: No authentication required (AllowAnonymous attribute).
+/// </example>
+/// <typeparam name="TRequest">The type of the request payload, expected to be a <see cref="UserRequest"/>.</typeparam>
+/// <typeparam name="TResponse">The type of the response payload, which is a <see cref="UserResponse"/> containing user details.</typeparam>
 public class UserRegistrationEndpoint(
     IUserRepository userRepository,
     IMapper<UserRequest, UserResponse, User> userMapper,
