9fe69bee8d7133957504fc371b4a9be61cd7e497
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-10-24 09:02:34 +0200

    Added Response DTO

---
+++ Server/src/Features/Users/DTOs/UserResponse.cs
@@ -0,0 +1,13 @@
+namespace src.Features.Users.DTOs;
+
+public record UserResponse
+{
+    public UserId Id { get; init; }
+    public string? Username { get; set; }
+    public string? Firstname { get; set; }
+    public string? Lastname { get; set; }
+    public string? Email { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+    public bool IsAdminUser { get; set; }
+}
