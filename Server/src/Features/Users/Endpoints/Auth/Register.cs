aa69115a9716617e1ddcd603546042fadef73348
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-10-24 09:05:15 +0200

    Added Registration endpoint

---
+++ Server/src/Features/Users/Endpoints/Auth/Register.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Primitives;
+using src.Features.Shared.Endpoints;
+using src.Features.Shared.Interfaces;
+using src.Features.Users.DTOs;
+using src.Utilities;
+
+namespace src.Features.Users.Endpoints.Auth;
+
+public class Register(IHttpContextAccessor accessor) : BaseEndpoint<UserRequest, UserResponse>(accessor), IEndpoint
+{
+    public override void Configure(IEndpointRouteBuilder app)
+    {
+        app.MapPost("users/register", HandleAsync)
+            .WithName("Register")
+            .AllowAnonymous()
+            .WithTags(Tags.Auth);
+    }
+
+    protected override (bool IsValid, IDictionary<string, string[]> Errors) ValidateRequest(UserRequest request)
+    {
+        Dictionary<string, string[]> errors = [];
+        if (string.IsNullOrWhiteSpace(request.Email)) errors["email"] = ["Email is required."];
+        if (string.IsNullOrWhiteSpace(request.Password)) errors["password"] = ["Password is required"];
+        return (errors.Count == 0, errors);
+    }
+
+    private async Task<IResult> HandleAsync(UserRequest request, CancellationToken ct)
+    {
+        return await ExecuteAsync(
+            request,
+            action: async cancellationToken =>
+            {
+                var mapper = GetRequired<IMapper<UserRequest, UserResponse, User>>();
+                UserRepository repository = GetRequired<UserRepository>();
+
+                User user = mapper.ToEntity(request);
+                user.Id = Users.UserId.NewId;
+                user.Created = DateTime.UtcNow;
+                user.Updated = DateTime.UtcNow;
+                user.IsAdminUser = false;
+                user.HashedPassword = BCrypt.Net.BCrypt.EnhancedHashPassword(request.Password);
+
+                User? addedUser = await repository.AddAsync(user);
+                if (addedUser is null)
+                    BadRequest("Failed to register user");
+
+                UserResponse response = mapper.ToResponse(addedUser!);
+
+                StringSegment etag = ComputeETag(response);
+                SetETag(etag);
+
+                return Created($"/users/{response.Id.Value}", response);
+            }, ct: ct);
+    }
+}
