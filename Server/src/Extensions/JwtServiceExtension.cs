d93fb9ca51297cf90741cd99fec30318b183f7c8
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-10-24 11:01:47 +0200

    Added JWT service extension for future JWT implementation

---
+++ Server/src/Extensions/JwtServiceExtension.cs
@@ -0,0 +1,9 @@
+namespace src.Extensions;
+
+public static class JwtServiceExtension
+{
+    public static void AddJwtServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        throw new NotImplementedException();
+    }
+}
