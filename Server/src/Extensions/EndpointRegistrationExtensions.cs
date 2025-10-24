2e6157ff81f6100036fb8e71749418f3ad1e7b30
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-10-24 09:02:56 +0200

    Added automatic Endpoint mapping

---
+++ Server/src/Extensions/EndpointRegistrationExtensions.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using src.Features.Shared.Endpoints;
+using src.Utilities;
+
+namespace src.Extensions;
+
+public static class EndpointRegistrationExtensions
+{
+    public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly? assembly = null)
+    {
+        assembly ??= Types.Program.Assembly;
+        
+        ServiceDescriptor[] endpointServiceDescriptors = assembly
+            .DefinedTypes
+            .Where(type => type is { IsAbstract: false, IsInterface: false } &&
+                           type.IsAssignableTo(Types.IEndpoint))
+            .Select(type => ServiceDescriptor.Transient(Types.IEndpoint, type))
+            .ToArray();
+        
+        services.TryAddEnumerable(endpointServiceDescriptors);
+
+        return services;
+    }
+
+    public static IApplicationBuilder MapEndpoints(this WebApplication app, RouteGroupBuilder? routeGroupBuilder = null)
+    {
+        IEnumerable<IEndpoint> endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+
+        IEndpointRouteBuilder builder = routeGroupBuilder is null ? app : routeGroupBuilder;
+
+        foreach (IEndpoint endpoint in endpoints)
+        {
+            endpoint.Configure(builder);
+        }
+
+        return app;
+    }
+}
