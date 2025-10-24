263ed3c0af79c5a9281cb51083b78477235196c3
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-10-24 08:55:52 +0200

    Added UserMapper

---
+++ Server/src/Features/Users/UserMapper.cs
@@ -0,0 +1,30 @@
+using src.Features.Shared.Interfaces;
+using src.Features.Users.DTOs;
+
+namespace src.Features.Users;
+
+public class UserMapper : IMapper<UserRequest, UserResponse, User>
+{
+    public User ToEntity(UserRequest request)
+    {
+        return new User
+        {
+            Username = request.Username,
+            Firstname = request.Firstname,
+            Lastname = request.Lastname,
+            Email = request.Email
+        };
+    }
+
+    public UserResponse ToResponse(User entity) => new()
+        {
+            Id = entity.Id,
+            Username = entity.Username,
+            Firstname = entity.Firstname,
+            Lastname = entity.Lastname,
+            Email = entity.Email,
+            Created = entity.Created,
+            Updated = entity.Updated,
+            IsAdminUser = entity.IsAdminUser
+        };
+}
