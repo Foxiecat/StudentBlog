785e3666b2ff6a5bfcc84a89df11941422f23f0a
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-10-27 09:29:09 +0100

    Added code: JwtMiddleware

--- Server/src/Middleware/JwtMiddleware.cs
+++ Server/src/Middleware/JwtMiddleware.cs
@@ -1,6 +1,24 @@
+using src.Services.Interfaces;
+
 namespace src.Middleware;
 
-public class JwtMiddleware
+public class JwtMiddleware(
+    ITokenService tokenService, 
+    ILogger<JwtMiddleware> logger) : IMiddleware
 {
-    
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        string? token = context.Request.Headers.Authorization.FirstOrDefault()?.Split(" ").Last();
+
+        if (token is not null)
+        {
+            (string? userId, IEnumerable<string>? roles) = tokenService.ValidateAccessToken(token);
+            logger.LogInformation("User: {UserId}, Roles: {roles}", userId, roles);
+
+            context.Items["UserId"] = userId;
+            context.Items["Roles"] = roles;
+        }
+
+        await next(context);
+    }
 }
