7802d62f173ecbf4c1bd5bb12be9c86fea5955d8
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-10-30 12:32:56 +0100

    Added: CRUD operations to base

--- Server/src/Features/Shared/Interfaces/IBaseRepository.cs
+++ Server/src/Features/Shared/Interfaces/IBaseRepository.cs
@@ -1,6 +1,13 @@
+using System.Linq.Expressions;
+
 namespace src.Features.Shared.Interfaces;
 
 public interface IBaseRepository<TEntity>
 {
     Task<TEntity?> AddAsync(TEntity entity);
+    Task<TEntity?> GetByIdAsync(Guid id);
+    Task<IEnumerable<TEntity>> GetPagedAsync(int pageIndex, int pageSize);
+    Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<TEntity?> UpdateAsync(TEntity entity);
+    Task<TEntity?> DeleteByIdAsync(Guid id);
 }
