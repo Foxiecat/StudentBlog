7f13f53a5bfd21a240e9f4fc619c3785be0511c5
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-10-30 12:04:40 +0100

    Fixed HTTPS redirection

--- Server/src/Program.cs
+++ Server/src/Program.cs
@@ -5,77 +5,63 @@ using src.Database;
 using src.Extensions;
 using src.Middleware;
 
-namespace src;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Log.Logger = new LoggerConfiguration()
+Log.Logger = new LoggerConfiguration()
             .CreateBootstrapLogger();
-        
-        try
-        {
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.RegisterRepositories();
-            builder.Services.RegisterMappers();
-
-            builder.Services.AddEndpoints();
-
-            builder.Services.AddProblemDetails();
-            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
             
-            //builder.Services.AddJwtServices(builder.Configuration);
-
-            builder.Services.AddDbContext<StudentBlogDbContext>(options => 
-                options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
-
-            builder.Host.UseSerilog((context, config) => 
-                config.ReadFrom.Configuration(context.Configuration));
-
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-            builder.Services.AddHttpContextAccessor();
-
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            WebApplication app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-                app.MapScalarApiReference();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseExceptionHandler();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
             
-            app.UseAuthorization();
+builder.Services.AddOpenApi();
+builder.Services.RegisterRepositories();
+builder.Services.RegisterMappers();
+builder.Services.AddEndpoints();
 
-            /*ApiVersionSet apiVersionSet = app.NewApiVersionSet()
-                .HasApiVersion(new ApiVersion(1))
-                .ReportApiVersions()
-                .Build();
+builder.Services.AddHttpContextAccessor();
 
-            RouteGroupBuilder versionedGroup = app
-                .MapGroup("api/v{version:apiVersion}")
-                .WithApiVersionSet(apiVersionSet);*/
-            
-            app.MapEndpoints();
+builder.Services.AddJwtServices(builder.Configuration);
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
-            app.Run();
-        }
-        catch (Exception e)
-        {
-            Log.Fatal(e, "Application Terminated Unexpectedly");
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
-    }
+builder.Services.AddDbContext<StudentBlogDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection"),
+        optionsBuilder => optionsBuilder.MigrationsAssembly("src"));
+});
+
+builder.Host.UseSerilog((context, config) => 
+    config.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddHttpsRedirection(options => options.HttpsPort = 8081);
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+
+    StudentBlogDbContext application = app.Services.CreateScope().ServiceProvider.GetRequiredService<StudentBlogDbContext>();
+    IEnumerable<string>? pendingMigrations = await application.Database.GetPendingMigrationsAsync();
+    if (!pendingMigrations.Contains(null))
+        await application.Database.MigrateAsync();
 }
+
+app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
+app.UseAuthentication();
+app.UseAuthorization();
+
+/*ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+    .HasApiVersion(new ApiVersion(1))
+    .ReportApiVersions()
+    .Build();
+
+RouteGroupBuilder versionedGroup = app
+    .MapGroup("api/v{version:apiVersion}")
+    .WithApiVersionSet(apiVersionSet);*/
+            
+app.MapEndpoints();
+
+app.Run();
